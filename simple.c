01e74e5e5a285dd523bb5b291a5c2e26b8ad03e7
Don Vu <don.vu1212@gmail.com>
2017-09-10 23:06:33 -0600

    Add the birthday structure to simple.c

--- simple.c
+++ simple.c
@@ -1,6 +1,14 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/list.h>
+
+struct birthday {
+	int day;
+	int month;
+	int year;
+	struct list_head list; /* kernel's list structure */
+};
 
 /* The initial calling function */
 int simple_init(void)
